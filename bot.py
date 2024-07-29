8826190395d8d1865adc24093182e9ac17e45731
Silicon Developer <166461734+Silicon-Developer@users.noreply.github.com>
2024-07-29 10:48:01 +0530

    Update

---
+++ bot.py
@@ -0,0 +1,24 @@
+from pyrogram import Client
+from info import *
+
+
+class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            name="Auto Cap",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            workers=200,
+            plugins={"root": "body"},
+            sleep_threshold=15,
+        )
+
+    async def start(self):
+        await super().start()
+        me = await self.get_me()
+        print(f"{me.first_name} Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️")
+        await self.send_message(ADMIN, f"**{me.first_name}  Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️**")
+
+
+Bot().run()
