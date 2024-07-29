b83c46265e0d27fda3a01e95efc18239f242d8d5
Silicon Developer <166461734+Silicon-Developer@users.noreply.github.com>
2024-07-29 20:35:13 +0530

    ALL OK

---
+++ body/Caption.py
@@ -0,0 +1,139 @@
+from pyrogram import *
+from info import *
+import asyncio
+from Script import script
+from .database import *
+import re
+from pyrogram.errors import FloodWait
+from pyrogram.types import *
+
+@Client.on_message(filters.command("start") & filters.private)
+async def strtCap(bot, message):
+    keyboard = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton("➕️ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ➕️", url=f"https://t.me/CustomCaptionBot?startchannel=true")
+            ],[
+                InlineKeyboardButton("Hᴇʟᴘ", callback_data="help"),
+                InlineKeyboardButton("Aʙᴏᴜᴛ", callback_data="about")
+            ],[
+                InlineKeyboardButton("🌐 Uᴘᴅᴀᴛᴇ", url=f"https://t.me/Silicon_Bot_Update"),
+                InlineKeyboardButton("📜 Sᴜᴘᴘᴏʀᴛ", url=r"https://t.me/Silicon_Botz")
+        ]
+    )
+    return await message.reply(
+        f"<b>Hᴇʟʟᴏ {message.from_user.mention}\n\nɪ ᴀᴍ ᴀᴜᴛᴏ ᴄᴀᴘᴛɪᴏɴ ʙᴏᴛ ᴡɪᴛʜ ᴄᴜsᴛᴏᴍ ᴄᴀᴘᴛɪᴏɴ.\n\nFᴏʀ ᴍᴏʀᴇ ɪɴғᴏ ʜᴏᴡ ᴛᴏ ᴜsᴇ ᴍᴇ ᴄʟɪᴄᴋ ᴏɴ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴ ɢɪᴠᴇɴ ʙᴇʟᴏᴡ.\n\nMᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ »<a href='https://t.me/Silicon_Bot_Update'>Sɪʟɪᴄᴏɴ Bᴏᴛᴢ</a></b>",
+        reply_markup=keyboard
+    )
+
+@Client.on_message(filters.command("set_cap") & filters.channel)
+async def setCap(bot, message):
+    if len(message.command) < 2:
+        return await message.reply(
+            "Usᴀɢᴇ: **/set_cap 𝑌𝑜𝑢𝑟 𝑐𝑎𝑝𝑡𝑖𝑜𝑛 𝑈𝑠𝑒 <code>{file_name}</code> 𝑇𝑜 𝑠ℎ𝑜𝑤 𝑦𝑜𝑢𝑟 𝐹𝑖𝑙𝑒 𝑁𝑎𝑚𝑒.\n\n𝑈𝑠𝑒<code>{file_size}</code> 𝑇𝑜 𝑠ℎ𝑜𝑤 𝑦𝑜𝑢𝑟 𝐹𝑖𝑙𝑒 𝑆𝑖𝑧𝑒/n/n✓ 𝑀𝑎𝑦 𝐵𝑒 𝑁𝑜𝑤 𝑌𝑜𝑢 𝑎𝑟𝑒 𝑐𝑙𝑒𝑎𝑟💫**"
+        )
+    chnl_id = message.chat.id
+    caption = (
+        message.text.split(" ", 1)[1] if len(message.text.split(" ", 1)) > 1 else None
+    )
+    chkData = await chnl_ids.find_one({"chnl_id": chnl_id})
+    if chkData:
+        await updateCap(chnl_id, caption)
+        return await message.reply(f"Your New Caption: {caption}")
+    else:
+        await addCap(chnl_id, caption)
+        return await message.reply(f"Yᴏᴜʀ Nᴇᴡ Cᴀᴘᴛɪᴏɴ Is: {caption}")
+
+
+@Client.on_message(filters.command("del_cap") & filters.channel)
+async def delCap(_, msg):
+    chnl_id = msg.chat.id
+    try:
+        await chnl_ids.delete_one({"chnl_id": chnl_id})
+        return await msg.reply("<b><i>✓ Sᴜᴄᴄᴇssғᴜʟʟʏ... Dᴇʟᴇᴛᴇᴅ Yᴏᴜʀ Cᴀᴘᴛɪᴏɴ Nᴏᴡ I ᴀᴍ Usɪɴɢ Mʏ Dᴇғᴀᴜʟᴛ Cᴀᴘᴛɪᴏɴ </i></b>")
+    except Exception as e:
+        e_val = await msg.replay(f"ERR I GOT: {e}")
+        await asyncio.sleep(5)
+        await e_val.delete()
+        return
+
+
+@Client.on_message(filters.channel)
+async def reCap(bot, message):
+    chnl_id = message.chat.id
+    if message.media:
+        for file_type in ("video", "audio", "document", "voice"):
+            obj = getattr(message, file_type, None)
+            if obj and hasattr(obj, "file_name"):
+                file_name = obj.file_name
+                file_size = obj.file_size
+                file_name = (
+                    re.sub(r"@\w+\s*", "", file_name)
+                    .replace("_", " ")
+                    .replace(".", " ")
+                )
+                cap_dets = await chnl_ids.find_one({"chnl_id": chnl_id})
+                try:
+                    if cap_dets:
+                        cap = cap_dets["caption"]
+                        replaced_caption = cap.format(file_name=file_name, file_size=get_size(file_size))
+                        await message.edit(replaced_caption)
+                    else:
+                        replaced_caption = DEF_CAP.format(file_name=file_name, file_size=get_size(file_size))
+                        await message.edit(replaced_caption)
+                except FloodWait as e:
+                    await asyncio.sleep(e.x)
+                    continue
+    return
+
+# Size conversion function
+def get_size(size):
+    units = ["Bytes", "Kʙ", "Mʙ", "Gʙ", "Tʙ", "Pʙ", "Eʙ"]
+    size = float(size)
+    i = 0
+    while size >= 1024.0 and i < len(units) - 1:  # Changed the condition to stop at the last unit
+        i += 1
+        size /= 1024.0
+    return "%.2f %s" % (size, units[i])
+
+@Client.on_callback_query(filters.regex(r'^start'))
+async def start(bot, query):
+    await query.message.edit_text(
+        text=script.START_TXT.format(query.from_user.mention),  
+        reply_markup=InlineKeyboardMarkup(
+            [[
+                InlineKeyboardButton("➕️ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ➕️", url=f"http://t.me/CustomCaptionBot?startchannel=true")
+                ],[
+                InlineKeyboardButton("Hᴇʟᴘ", callback_data="help"),
+                InlineKeyboardButton("Aʙᴏᴜᴛ", callback_data="about")
+            ],[
+                InlineKeyboardButton("🌐 Uᴘᴅᴀᴛᴇ", url=f"https://t.me/Silicon_Bot_Update"),
+                InlineKeyboardButton("📜 Sᴜᴘᴘᴏʀᴛ", url=r"https://t.me/Silicon_Botz")
+            ]
+        )
+)
+
+@Client.on_callback_query(filters.regex(r'^help'))
+async def help(bot, query):
+    await query.message.edit_text(
+        text=script.HELP_TXT,
+        reply_markup=InlineKeyboardMarkup(
+            [[
+            InlineKeyboardButton('About', callback_data='about')
+            ],[
+            InlineKeyboardButton('↩ ʙᴀᴄᴋ', callback_data='start')
+            ]]
+        ))
+
+
+@Client.on_callback_query(filters.regex(r'^about'))
+async def about(bot, query):
+    await query.message.edit_text(
+        text=script.ABOUT_TXT,
+        reply_markup=InlineKeyboardMarkup(
+            [[
+            InlineKeyboardButton('ʜᴏᴡ ᴛᴏ ᴜsᴇ ᴍᴇ ❓', callback_data='help')
+            ],[
+            InlineKeyboardButton('↩ ʙᴀᴄᴋ', callback_data='start')
+            ]]
+        ))
