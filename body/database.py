4b775a72ed37e29d72f5b71b1ab6cec06ac8e301
Silicon Developer <166461734+Silicon-Developer@users.noreply.github.com>
2024-11-11 09:22:15 +0530

    Update database.py

--- body/database.py
+++ body/database.py
@@ -4,7 +4,7 @@ from info import *
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DB)
 db = client.captions_with_chnl
 chnl_ids = db.chnl_ids
-
+users = db.users
 
 async def addCap(chnl_id, caption):
     dets = {"chnl_id": chnl_id, "caption": caption}
@@ -13,3 +13,21 @@ async def addCap(chnl_id, caption):
 
 async def updateCap(chnl_id, caption):
     await chnl_ids.update_one({"chnl_id": chnl_id}, {"$set": {"caption": caption}})
+
+async def insert(user_id):
+    user_det = {"_id": user_id}
+    try:
+        await users.insert_one(user_det)
+    except:
+        pass
+        
+async def total_user():
+    user = await users.count_documents({})
+    return user
+
+async def getid():
+    all_users = users.find({})
+    return all_users
+
+async def delete(id):
+    await users.delete_one(id)
